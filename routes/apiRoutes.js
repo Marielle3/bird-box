92036ed7a0e5a8c6af61e5f95c740801a2a89c93
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-11-02 10:30:15 -0700

    adding some style and working on getting logged in

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,9 +10,9 @@ module.exports = function(app) {
 
   app.post("/api/signUp", function(req, res) {
     console.log("this is signup");
-    db.Registration.create(req.body).then(function(dbRegistration) {
-      console.log("registration", dbRegistration);
-      res.json(dbRegistration);
+    db.User.create(req.body).then(function(dbUser) {
+      console.log("registration", dbUser);
+      res.json(dbUser);
     });
   });
   // Create a new example
